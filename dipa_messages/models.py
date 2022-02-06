ddd75130859230ac9590bf590fc35251ad5a96e1
Vitaly Kalinin <v.kalinin@emzior.ru>
2022-02-06 18:14:21 +0300

    str method added to the messagemodel

--- dipa_messages/models.py
+++ dipa_messages/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class MessageModel(models.Model):
     email = models.EmailField(max_length=150, default="anonym@dipa.ru")
     message = models.CharField(max_length=250, default="")
+
+    def __str__(self):
+        return f"sender: {self.email}, the message: {self.message}"
